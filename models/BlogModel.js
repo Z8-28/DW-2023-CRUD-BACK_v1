e965cf12048689a4924c2350ef5028d7e6f2606c
Z8-28 <josealejandroquiroa@gmail.com>
2023-09-28 18:22:12 -0600

    hola

---
+++ models/BlogModel.js
@@ -0,0 +1,12 @@
+//Importamos la conexion a la base de datos
+import db from "../database/db.js";
+//Importamos sequelize
+import { DataTypes } from "sequelize";
+
+const BlogModel = db.define('tareas', {
+    title: {type: DataTypes.STRING},
+    content: {type: DataTypes.STRING},
+
+})
+
+export default BlogModel
